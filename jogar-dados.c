63a097ade0b537cb522bb224bc3dcd146e5cc746
Gustavo Soares <gustavosilvasoareslima@gmail.com>
2022-05-20 21:03:50 -0300

    Jogo dados mais bonito

--- jogar-dados.c
+++ jogar-dados.c
@@ -7,49 +7,65 @@ int main(){
   time_t timeNumber;
   srand ( (unsigned) time (&timeNumber));
 
-  int rodadas, l1 = 0, l2 = 0, l3 = 0, l4 = 0, l5 = 0, l6 = 0;
+  int rodadas, l1 = 0, l2 = 0, l3 = 0, l4 = 0, l5 = 0, l6 = 0, pressionado;
 
   printf("Digite a quantidade de vezes que deseja rodar o dado: ");
   scanf("%d", &rodadas);
+  getchar();
 
   for(int i = 0; i <  rodadas; i++){
-    switch ((rand() % 5) + 1)
+    switch ((rand() % 6) + 1)
     {
     case 1:
       l1++;
-      printf(" _____\n|     |\n|  @  |\n|_____|\n");
+      printf(" _____%d\n|     |\n|  @  |\n|_____|\n", l1);
+      getchar();
+      system("clear");
       break;
     case 2:
       l2++;
-      printf(" _____\n|@    |\n|     |\n|____@|\n");
+      printf(" _____%d\n|@    |\n|     |\n|____@|\n", l2);
+      getchar();
+      system("clear");
       break;
     case 3:
       l3++;
-      printf(" _____\n|@    |\n|  @  |\n|____@|\n");
+      printf(" _____%d\n|@    |\n|  @  |\n|____@|\n", l3);
+      getchar();
+      system("clear");
       break;
     case 4:
       l4++;
-      printf(" _____\n|@   @|\n|     |\n|@___@|\n");
+      printf(" _____%d\n|@   @|\n|     |\n|@___@|\n", l4);
+      getchar();
+      system("clear");
       break;
     case 5:
       l5++;
-      printf(" _____\n|@   @|\n|  @  |\n|@___@|\n");
+      printf(" _____%d\n|@   @|\n|  @  |\n|@___@|\n", l5);
+      getchar();
+      system("clear");
       break;
     
     default:
       l6++;
-      printf(" _____\n|@   @|\n|@   @|\n|@___@|\n");
+      printf(" _____%d\n|@   @|\n|@   @|\n|@___@|\n", l6);
+      getchar();
+      system("clear");
       break;
     }
   }
 
-  printf("\nForam sorteadas as faces de numero:\n");
-  printf(" - 1: %d vezes sendo isso (%.2f)%% do total\n", l1, (l1/(float)rodadas)*100);
-  printf(" - 2: %d vezes sendo isso (%.2f)%% do total\n", l2, (l2/(float)rodadas)*100);
-  printf(" - 3: %d vezes sendo isso (%.2f)%% do total\n", l3, (l3/(float)rodadas)*100);
-  printf(" - 4: %d vezes sendo isso (%.2f)%% do total\n", l4, (l4/(float)rodadas)*100);
-  printf(" - 5: %d vezes sendo isso (%.2f)%% do total\n", l5, (l5/(float)rodadas)*100);
-  printf(" - 6: %d vezes sendo isso (%.2f)%% do total\n", l6, (l6/(float)rodadas)*100);
+  printf(" ___________________________________ \n");
+  printf("| Foram sorteadas as faces:         |\n");
+  printf("|===================================|\n");
+  printf("| 1: %dx sendo isso %06.2f%% do total |\n", l1, (l1/(float)rodadas)*100);
+  printf("| 2: %dx sendo isso %06.2f%% do total |\n", l2, (l2/(float)rodadas)*100);
+  printf("| 3: %dx sendo isso %06.2f%% do total |\n", l3, (l3/(float)rodadas)*100);
+  printf("| 4: %dx sendo isso %06.2f%% do total |\n", l4, (l4/(float)rodadas)*100);
+  printf("| 5: %dx sendo isso %06.2f%% do total |\n", l5, (l5/(float)rodadas)*100);
+  printf("| 6: %dx sendo isso %06.2f%% do total |\n", l6, (l6/(float)rodadas)*100);
+  printf("|###################################|\n");
 
   return 0;
 }
